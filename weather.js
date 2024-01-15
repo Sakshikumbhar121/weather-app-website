fd3566d9bdf154833a6897809ea796825a894e0b
Sakshikumbhar121 <sakshikumbhar237@gmail.com>
2024-01-15 15:06:41 +0530

    Second commit

---
+++ weather.js
@@ -0,0 +1,33 @@
+const apiKey = "af47b352098cfc99092139eabf61da33";
+const apiUrl = "https://api.openweathermap.org/data/2.5/weather?&units=metric&q=";
+
+const searchBox = document.querySelector(".search input");
+const searchBtn = document.querySelector(".search button");
+const weatherIcon = document.querySelector(".weather-icon");
+
+async function checkWeather(city) {
+  const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
+
+  if (response.status == 404) {
+    document.querySelector(".error").style.display = "block";
+    document.querySelector(".weather").style.display = "none";
+  } else {
+    const data = await response.json();
+
+    document.querySelector(".city").innerHTML = data.name;
+    document.querySelector(".temp").innerHTML = Math.round(data.main.temp) + "°C";
+    document.querySelector(".humidity").innerHTML = data.main.humidity + "%";
+    document.querySelector(".wind").innerHTML = data.wind.speed + "km/h";
+
+    // Set the weather icon based on the weather condition
+    const weatherMain = data.weather[0].main.toLowerCase();
+    weatherIcon.src = `img/${weatherMain}.png`;
+
+    document.querySelector(".weather").style.display = "block";
+    document.querySelector(".error").style.display = "none";
+  }
+}
+
+searchBtn.addEventListener("click", () => {
+  checkWeather(searchBox.value);
+});
